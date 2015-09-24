b3de094ef80257162a3d0623031bf04e4ce5f83c
Gott Phusit <saranyu_phusit@hotmail.com>
2015-09-24 04:52:21 -0400

    Add listing page with mock data (still at home URL)

--- sparrow-web/app/js/components/Header.js
+++ sparrow-web/app/js/components/Header.js
@@ -8,15 +8,18 @@ const Header = React.createClass({
   render() {
     return (
       <header>
-        <div className="logo">
-          <h1>Sparrow</h1>
-          <p>Lend and borrow</p>
-        </div>
-        <div className="tabs">
-          <Link to="/" className="tab active">Borrow Requests</Link>
-          <Link to="/" className="tab">Lend Offers</Link>
-        </div>
+        <div className="wrapper">
+          <div className="logo">
+            <h1>Sparrow</h1>
+            <p>Lend and borrow</p>
+          </div>
+          <div className="tabs">
+            <Link to="/" className="tab active">Borrow Requests</Link>
+            <Link to="/" className="tab">Lend Offers</Link>
+          </div>
           <div className="login">
+            <Link to="/login" className="login-btn">Login</Link>
+          </div>
         </div>
       </header>
     );
--- sparrow-web/app/js/pages/HomePage.js
+++ sparrow-web/app/js/pages/HomePage.js
@@ -4,25 +4,63 @@ import React         from 'react/addons';
 import {Link}        from 'react-router';
 import DocumentTitle from 'react-document-title';
 
+import ListItem from '../components/ListItem';
+
 const HomePage = React.createClass({
 
+  getInitialState() {
+    return {
+      mockData: [
+        {
+          name: 'birdy',
+          time: '1 min ago',
+          text: 'Can I please borrow a nest?',
+          duration: '2 hours',
+          price: '$0-$20'
+        },
+        {
+          name: 'eagle',
+          time: '3 mins ago',
+          text: 'Need a glasses',
+          duration: '1 day',
+          price: 'free'
+        },
+        {
+          name: 'albatross',
+          time: '2 days later',
+          text: 'I don\'t need you',
+          duration: '1 second',
+          price: '$30000'
+        }
+      ]
+    }
+  },
+  
   propTypes: {
     currentUser: React.PropTypes.object.isRequired
   },
 
+  mockList() {
+    return (
+      <div className="list-container">
+        {
+          this.state.mockData.map((item) => {
+            return <ListItem item={item}/>
+          })
+        }
+      </div>
+    )
+  },
+  
   render() {
     return (
       <DocumentTitle title="Sparrow">
         <section className="home-page">
 
-          <div>
-            Home
+          <div className="wrapper"> 
+            {this.mockList()}
           </div>
-
-          <div>
-            <Link to="/search">Search</Link>
-          </div>
-
+          
         </section>
       </DocumentTitle>
     );
