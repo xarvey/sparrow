a3baee44eb5ff231e967091749c82af71423782a
Sargun Vohra <sargunster@gmail.com>
2015-09-23 21:07:04 -0400

    handle exceptions in listing properly

--- sparrow-server/src/main/kotlin/io/github/cmdq/sparrow/server/endpoints/listings.kt
+++ sparrow-server/src/main/kotlin/io/github/cmdq/sparrow/server/endpoints/listings.kt
@@ -1,7 +1,6 @@
 package io.github.cmdq.sparrow.server.endpoints
 
 import com.google.gson.Gson
-import com.google.gson.JsonParseException
 import io.github.cmdq.sparrow.server.Listing
 import io.github.cmdq.sparrow.server.Sparrow
 import io.github.cmdq.sparrow.server.toJson
@@ -20,15 +19,10 @@ fun listings(service: Sparrow) {
     }
 
     Spark.put("/$dir") { request, response ->
-        try {
-            val listing: Listing = request.body().toObject(gson)
-            val result = service.listings.editListing(listing)
-            response.status(result.status)
-            result.body.toJson(gson)
-        } catch (e: JsonParseException) {
-            response.status(400)
-            "Bad request".toJson(gson)
-        }
+        val listing: Listing = request.body().toObject(gson)
+        val result = service.listings.editListing(listing)
+        response.status(result.status)
+        result.body.toJson(gson)
     }
 
     Spark.put("/$dir/borrow/filter") { request, response ->
@@ -44,15 +38,10 @@ fun listings(service: Sparrow) {
     }
 
     Spark.post("/$dir/lend") { request, response ->
-        try {
-            val listing: Listing = request.body().toObject(gson)
-            val result = service.listings.createLendListing(listing)
-            response.status(result.status)
-            result.body.toJson(gson)
-        } catch(e: JsonParseException) {
-            response.status(400)
-            "Bad request".toJson(gson)
-        }
+        val listing: Listing = request.body().toObject(gson)
+        val result = service.listings.createLendListing(listing)
+        response.status(result.status)
+        result.body.toJson(gson)
     }
 
     Spark.delete("/$dir/id") { request, response ->
