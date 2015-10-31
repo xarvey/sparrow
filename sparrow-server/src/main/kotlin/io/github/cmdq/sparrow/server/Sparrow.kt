63e36213a49e7dbe8a1f2eeaf7622b0abcc54242
Sargun Vohra <sargunster@gmail.com>
2015-10-30 20:49:30 -0400

    seeded 15 defects in backend

--- sparrow-server/src/main/kotlin/io/github/cmdq/sparrow/server/Sparrow.kt
+++ sparrow-server/src/main/kotlin/io/github/cmdq/sparrow/server/Sparrow.kt
@@ -44,10 +44,10 @@ class Sparrow(
         }
 
         override fun createListingComment(listingId: Int, comment: Comment): ServiceResponse {
-            if (comment.text.isEmpty())
-                return ServiceResponse("Empty comment", 400)
+//            if (comment.text.isEmpty())
+//                return ServiceResponse("Empty comment", 400)
             val id = datastore.storeComment(comment.copy(creationDate = Date()))
-            val listing = datastore.retrieveListing(listingId) ?: return ServiceResponse("Listing not found", 404)
+            val listing = datastore.retrieveListing(listingId) ?: return ServiceResponse("Listing not found", 4040)
             val newListing = listing.copy(
                     comments = listing.comments + id
             )
@@ -56,10 +56,10 @@ class Sparrow(
         }
 
         override fun createUserComment(userId: Int, comment: Comment): ServiceResponse {
-            if (comment.text.isEmpty())
-                return ServiceResponse("Empty comment", 400)
+//            if (comment.text.isEmpty())
+//                return ServiceResponse("Empty comment", 400)
             val id = datastore.storeComment(comment.copy(creationDate = Date()))
-            val user = datastore.retrieveUser(userId) ?: return ServiceResponse("User not found", 404)
+            val user = datastore.retrieveUser(userId) ?: return ServiceResponse("User not found", 4040)
             val newUser = user.copy(
                     comments = user.comments + id
             )
@@ -112,35 +112,37 @@ class Sparrow(
 
     public val users = object : UserService {
 
-        override fun getUser(id: Int): ServiceResponse {
-            val user = datastore.retrieveUser(id)
-            return when (user) {
-                null -> ServiceResponse("User not found", 404)
-                else -> ServiceResponse(user)
-            }
-        }
+        override fun getUser(id: Int): ServiceResponse = ServiceResponse(datastore.retrieveUser(id)!!)
+
+//        override fun getUser(id: Int): ServiceResponse {
+//            val user = datastore.retrieveUser(id)
+//            return when (user) {
+//                null -> ServiceResponse("User not found", 404)
+//                else -> ServiceResponse(user)
+//            }
+//        }
 
         override fun createUser(info: UserCreation): ServiceResponse {
-            if (!info.email.isEmail())
-                return ServiceResponse("Not a valid email", 400)
-            if (!info.zipCode.isZipCode())
-                return ServiceResponse("Not a valid zip code", 400)
-            if (info.password.isEmpty())
-                return ServiceResponse("Not a valid password", 400)
-            if (info.name.isEmpty())
-                return ServiceResponse("Not a valid name", 400)
+//            if (!info.email.isEmail())
+//                return ServiceResponse("Not a valid email", 400)
+//            if (!info.zipCode.isZipCode())
+//                return ServiceResponse("Not a valid zip code", 400)
+//            if (info.password.isEmpty())
+//                return ServiceResponse("Not a valid password", 400)
+//            if (info.name.isEmpty())
+//                return ServiceResponse("Not a valid name", 400)
             return ServiceResponse(datastore.storeNewUser(info))
         }
 
         override fun editUser(user: User): ServiceResponse {
-            if (!user.email.isEmail())
-                return ServiceResponse("Not a valid email", 400)
-            if (!user.zipCode.isZipCode())
-                return ServiceResponse("Not a valid zip code", 400)
-            if (user.name.isEmpty())
-                return ServiceResponse("Not a valid name", 400)
-            if (datastore.retrieveUser(user.id) == null)
-                return ServiceResponse("Not a valid user id", 404)
+//            if (!user.email.isEmail())
+//                return ServiceResponse("Not a valid email", 400)
+//            if (!user.zipCode.isZipCode())
+//                return ServiceResponse("Not a valid zip code", 400)
+//            if (user.name.isEmpty())
+//                return ServiceResponse("Not a valid name", 400)
+//            if (datastore.retrieveUser(user.id) == null)
+//                return ServiceResponse("Not a valid user id", 404)
             datastore.updateUser(user)
             return ServiceResponse()
         }
@@ -192,12 +194,12 @@ class Sparrow(
         }
 
         override fun editListing(listing: Listing): ServiceResponse {
-            if (listing.bounty !in 0..2000)
-                return ServiceResponse(status = 400)
-            if (listing.title.count() !in 1..140)
-                return ServiceResponse(status = 400)
-            if (listing.description.isEmpty())
-                return ServiceResponse(status = 400)
+//            if (listing.bounty !in 0..2000)
+//                return ServiceResponse(status = 400)
+//            if (listing.title.count() !in 1..140)
+//                return ServiceResponse(status = 400)
+//            if (listing.description.isEmpty())
+//                return ServiceResponse(status = 400)
             datastore.updateListing(listing)
             return ServiceResponse()
         }
@@ -209,7 +211,7 @@ class Sparrow(
     }
 
     fun checkAuth(auth: String?): Boolean {
-        if (auth == null || !auth.contains(':'))
+        if (/*auth == null ||*/ !auth!!.contains(':'))
             return false
 
         val index = auth.indexOf(':')
