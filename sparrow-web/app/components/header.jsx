f4a24ddadad9e3cf31372d7e9a0beffe90b771e5
xarvey <xiao67@purdue.edu>
2015-11-03 17:13:36 -0500

    fix bug

--- sparrow-web/app/components/header.jsx
+++ sparrow-web/app/components/header.jsx
@@ -46,12 +46,12 @@ class Header extends Component {
   logout() {
     var cookies = document.cookie.split(";");
 
-    for (var i = 0; i < cookies.length; i++) {
+    /*for (var i = 0; i < cookies.length; i++) {
       var cookie = cookies[i];
       var eqPos = cookie.indexOf("=");
       var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
       document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
+    }*/
     window.location.href="/borrow";
   }
 
